03257cc20e8b22bf045fbd0f3c89129d827855c7
Pier-Luc Richard <84756667+pirichar@users.noreply.github.com>
2025-04-28 22:28:40 -0400

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input1;
+string input2;
+int nmb1;
+int nmb2;
+int rtn = 0;
+char Operation = '\0';
+
+// first input
+Console.WriteLine("Hello! \nInput the first number: ");
+input1 = Console.ReadLine();
+try{
+    nmb1 = Int32.Parse(input1);
+}
+catch (FormatException) {
+    return; 
+}
+
+// second input
+Console.WriteLine("Input the second number: ");
+input2 = Console.ReadLine();
+try
+{
+    nmb2 = Int32.Parse(input2);
+}
+catch (FormatException)
+{
+    return;
+}
+
+//type chose
+Console.WriteLine("[a]dd numbers?");
+Console.WriteLine("[s]ubstract numbers?");
+Console.WriteLine("[m]ultiply numbers]");
+string calcul = Console.ReadLine();
+if (calcul != null)
+{
+    switch (calcul.ToUpper())
+    {
+        case "A":
+            Operation = '+';
+            rtn = (nmb1 + nmb2);
+            break;
+        case "S":
+            Operation = '-';
+            rtn = (nmb1 - nmb2);
+            break;
+        case "M":
+            Operation = '*';
+            rtn = (nmb1 * nmb2);
+            break;
+        default:
+            Console.WriteLine("invalid option");
+            Console.WriteLine("Press anykey to leave");
+            Console.ReadLine();
+            return;
+    }
+    Console.WriteLine($"{nmb1}{Operation}{nmb2}={rtn}");
+}
+else
+{
+    Console.WriteLine("Please provide a functionnal input");
+    return ;
+}
+
+Console.WriteLine("Press anykey to leave");
+Console.ReadKey();
+
+
+
+
+
---
+++ ToDoList/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Hello");
+Console.WriteLine("What do you want to do ?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+switch (userChoice.ToUpper())
+{
+    case "S":
+        PrintSelectedOpiton("See all TODOs");
+        break;
+    case "A":
+        PrintSelectedOpiton("Add a todo");
+        break;
+    case "R":
+        PrintSelectedOpiton("Remove a todo");
+        break;
+    case "E":
+        PrintSelectedOpiton("Exit");
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        break;
+
+}
+
+void PrintSelectedOpiton(string option)
+{
+    Console.WriteLine($"Selected option: {option}");
+}
+Console.ReadKey();
+
